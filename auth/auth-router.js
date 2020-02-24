7572a961bd0f64ecc687b506ef6b52522eda72f4
silvodesigns <kevsilvo@gmail.com>
2020-02-24 07:18:28 -0500

    writing users model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
   // implement registration
+
+  let user = req.body;
+
+  console.log(user, 'from inside register');
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+
 });
 
 router.post('/login', (req, res) => {
