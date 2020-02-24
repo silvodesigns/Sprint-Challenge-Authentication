547a85f02331e8ac3df23c0c361b8deaed651e06
silvodesigns <kevsilvo@gmail.com>
2020-02-24 07:40:51 -0500

    implementing login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,24 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+
+  let {username, password } = req.body;
+
+  Users.findBy({username})
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)){
+        
+          res.status(200).json({
+              message: `Welcome ${user.username}`
+          })
+      } else {
+          res.status(401).json({message:'Invalid Credentials'});
+      }
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
 });
 
 module.exports = router;
