5a61bca85e7de0d3d1b47876e08f171f4d9da193
Kevin Silvestre <kevsilvo@gmail.com>
2020-06-14 22:11:33 -0400

    register user done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,9 +2,28 @@ const router = require('express').Router();
 const hash = require('bcryptjs');
 const token = require('jsonwebtoken');
 
+const Users = require('../users/users-model.js');
+
 router.post('/register', (req, res) => {
   // implement registration
-  
+    const newUser = req.body;
+
+    if(newUser.username || newUser.password) {
+        const hashed =  hash.hashSync(newUser.password, 10);
+        newUser.password = hashed;
+
+        Users.insert(newUser)
+          .then( user => {
+              res.status(200).json({user})
+          })
+          .catch( err => {
+              res.status(200).json({err})
+          })
+
+    } else {
+      res.json({error: "Make sure to pass in username and password"});
+    }
+
 
 
 });
